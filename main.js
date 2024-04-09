7ed9ef864bad3989f81765b41e1a0530fbda82da
ang3lcr <ang3lcortes@outlook.com>
2024-04-08 18:52:53 -0700

    button styles

--- main.js
+++ main.js
@@ -3,16 +3,26 @@ const cardHorario = document.getElementById("card2")
 const button1 = document.getElementById("cursando");
 const button2 = document.getElementById("horario");
 cardHorario.classList.add("hidden")
+button1.classList.add("bg-violet-600")
+button1.classList.add("text-white")
 
 
 button1.onclick = () => {
     cardCursando.classList.remove('hidden');
     cardHorario.classList.add('hidden');
+    button1.classList.add("bg-violet-600")
+    button1.classList.add("text-white")
+    button2.classList.remove("bg-violet-600")
+    button2.classList.remove("text-white")
 }
 
 button2.onclick = () => {
     cardCursando.classList.add("hidden")
     cardHorario.classList.remove("hidden")
+    button2.classList.add("bg-violet-600")
+    button2.classList.add("text-white")
+    button1.classList.remove("bg-violet-600")
+    button1.classList.remove("text-white")
 }
 
 
